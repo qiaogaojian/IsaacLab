0890ad2eada316a8e39095e9e56de0f5bc6a1e1e
qiaogaojia <qiaogaojian@vip.qq.com>
2024-06-26 17:47:18 +0800

    feat: 实现 pai 机器人播放 动作序列

--- source/test/pai_play_actions.py
+++ source/test/pai_play_actions.py
@@ -4,6 +4,7 @@ This script demonstrates different legged robots.
     # Usage
     ./isaaclab.sh -p source/standalone/demos/quadrupeds.py
 """
+from pycore.utils.file_utils import FileUtils
 
 """Launch Isaac Sim Simulator first."""
 
@@ -26,7 +27,8 @@ simulation_app = app_launcher.app
 
 import numpy as np
 import torch
-
+import numpy as np
+import pandas as pd
 import omni.isaac.core.utils.prims as prim_utils
 
 import omni.isaac.lab.sim as sim_utils
@@ -36,8 +38,10 @@ from omni.isaac.lab.assets import Articulation
 # Pre-defined configs
 ##
 from omni.isaac.lab_assets.pai import PAI_CFG
-from omni.isaac.lab_assets.spot import SPOT_CFG  # isort:skip
-from omni.isaac.lab_assets.unitree import UNITREE_A1_CFG, UNITREE_GO1_CFG, UNITREE_GO2_CFG  # isort:skip
+
+from pycore.base import Core
+from pycore.logger import Logger
+from pycore.utils.tools import Tools
 
 
 def design_scene() -> tuple[dict, list[list[float]]]:
@@ -63,41 +67,52 @@ def run_simulator(sim: sim_utils.SimulationContext, robot: dict[str, Articulatio
     sim_dt = sim.get_physics_dt()
     sim_time = 0.0
     count = 0
+
+    # ********************* action queue *********************
+    # 使用pandas读取CSV文件
+    csv_file_path = FileUtils.get_project_path("resources/csv/action_Jun12_20-01-14_pos_v13.csv")
+    df = pd.read_csv(csv_file_path)
+    # 将DataFrame转换为numpy数组
+    np_array = df.values
+
+    action_queue = []
+    for row in np_array:
+        ten = torch.Tensor(row)
+        action = ten.unsqueeze(0)
+        action_queue.append(action)
+    print(f"load csv: {csv_file_path} success!")
+
     # Simulate physics
     while simulation_app.is_running():
         # reset
-        if count % 200 == 0:
-            # reset counters
-            sim_time = 0.0
-            count = 0
+        sim_time = 0.0
 
-            # root state
-            root_state = robot.data.default_root_state.clone()
-            # root_state[:, 2] += 1
-            robot.write_root_state_to_sim(root_state)
-            # joint state
-            joint_pos, joint_vel = robot.data.default_joint_pos.clone(), robot.data.default_joint_vel.clone()
-            robot.write_joint_state_to_sim(joint_pos, joint_vel)
-            # reset the internal state
-            robot.reset()
+        # root state
+        root_state = robot.data.default_root_state.clone()
+        # root_state[:, 2] += 1
+        robot.write_root_state_to_sim(root_state)
+        # joint state
+        joint_pos, joint_vel = robot.data.default_joint_pos.clone(), robot.data.default_joint_vel.clone()
+        robot.write_joint_state_to_sim(joint_pos, joint_vel)
+        # reset the internal state
+        robot.reset()
+        print("[INFO]: Resetting robots state...")
 
-            print("[INFO]: Resetting robots state...")
+        for action in action_queue:
+            # generate random joint positions
+            joint_pos_target = action[0] * 0.25
+            # apply action to the robot
+            robot.set_joint_position_target(joint_pos_target)
+            # write data to sim
+            robot.write_data_to_sim()
 
-        # generate random joint positions
-        joint_pos_target = robot.data.default_joint_pos + torch.randn_like(robot.data.joint_pos) * 0.9
-        # apply action to the robot
-        robot.set_joint_position_target(joint_pos_target)
-        # write data to sim
-        robot.write_data_to_sim()
+            # perform step
+            sim.step()
+            # update sim-time
+            sim_time += sim_dt
 
-        # perform step
-        sim.step()
-        # update sim-time
-        sim_time += sim_dt
-        count += 1
-
-        # update buffers
-        robot.update(sim_dt)
+            # update buffers
+            robot.update(sim_dt)
 
 
 def main():
@@ -116,7 +131,13 @@ def main():
     run_simulator(sim, robot)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    args = Tools.parse_args()
+    core = Core()
+    core.init(env=args.env)
+    Logger.instance().info('********************************* Test *********************************')
+
+
     # run the main function
     main()
     # close sim app
